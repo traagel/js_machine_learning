1e73751cc4d4cf9232e876618e183a7982202e78
mart <marttraagel@gmail.com>
2022-06-16 15:01:52 +0300

    driving works, sensors work

---
+++ car.js
@@ -0,0 +1,84 @@
+class Car{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.speed=0;
+        this.acceleration = 0.2;
+        this.maxSpeed=3;
+        this.friction = 0.05;
+        this.angle = 0;
+
+        this.sensor = new Sensor(this);
+
+        this.controls = new Controls();
+    }
+
+    update(roadBorders){
+        this.#move();
+        this.sensor.update(roadBorders);
+    }
+
+    #move(){
+        // controls logic - direction
+        if(this.controls.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controls.reverse){
+            this.speed-=this.acceleration;
+        }
+        // speed calculation (negative speed implies reversing)
+        // speed 1 - acceleration
+        if(this.speed>this.maxSpeed){
+            this.speed=this.maxSpeed;
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed=-this.maxSpeed/2;
+        }
+        // speed 2 - friction
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        // speed 3 - stop completely
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        //turning logic
+        if(this.speed != 0){
+            const flip=this.speed>0?1:-1;
+            if(this.controls.left){
+                this.angle+=0.03*flip;
+            }
+            if(this.controls.right){
+                this.angle-=0.03*flip;
+            }
+        }
+        //update x y position
+        this.y-=Math.cos(this.angle)*this.speed;
+        this.x-=Math.sin(this.angle)*this.speed;
+    }
+
+    draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+
+        ctx.beginPath();
+        ctx.rect(
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+
+        ctx.restore();
+
+        this.sensor.draw(ctx);
+    }
+}
