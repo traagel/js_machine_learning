151a097e9a940920013c6a96a9cee3ad07101158
mart <marttraagel@gmail.com>
2022-06-16 19:08:45 +0300

    neural net added

--- utils.js
+++ utils.js
@@ -7,7 +7,7 @@ function getIntersection(A,B,C,D){
     const uTop=(C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y);
     const bottom=(D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y);
 
-    if(bottom!=0){
+    if(bottom!==0){
         const t=tTop/bottom;
         const u=uTop/bottom;
         if(t>0 && t<=1 && u>=0 && u<=1){
@@ -19,4 +19,29 @@ function getIntersection(A,B,C,D){
         }
     }
     return null;
+}
+
+function polysIntersect(poly1, poly2){
+    for(let i=0;i<poly1.length;i++){
+        for(let j=0;j<poly2.length;j++){
+            const touch = getIntersection(
+                poly1[i],
+                poly1[(i+1) % poly1.length],
+                poly2[j],
+                poly2[(j+1) % poly2.length]
+            );
+            if(touch){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getRGBA(value){
+    const alpha=Math.abs(value);
+    const R=value<0?0:255;
+    const G=R;
+    const B=value>0?0:255;
+    return "rgba("+R+","+G+","+B+","+alpha+")";
 }
